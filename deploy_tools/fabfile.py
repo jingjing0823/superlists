2e1f5ce585c974f905fae3b6a8db7676671c79e8
lijing <lijing1@syswin.com>
2019-06-25 10:59:28 +0800

    update fabric file to change the collect static command

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -8,9 +8,9 @@ def deploy():
     site_folder=f'/home/{env.user}/sites/{env.host}'
     source_folder=site_folder+"/source"
     _create_directory_if_necessary(site_folder)
-    _get_latest_source(source_floder)
-    _update_setting(source_floder,env.host)
-    _update_virtualenv(source_floder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder,env.host)
+    _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
 
@@ -18,22 +18,22 @@ def _create_directory_if_necessary(site_folder):
     for subfolder in ('database','static','virtualenv','source'):
         run(f'mkdir -p {site_folder}/{subfolder}')
         
-def get_latest_source(source_folder):
+def _get_latest_source(source_folder):
     if exists(source_folder+"/.git"):
-        run(f'cd {souce_folder} && git fetch')
+        run(f'cd {source_folder} && git fetch')
     else:
         run(f'git clone {REPO_URL} {source_folder}')
     current_commit=local("git log -n 1 --format=%H",capture=True)
     run(f'cd {source_folder} && git reset --hard {current_commit}')
     
-def _upate_settings(source_folder,site_name):
+def _update_settings(source_folder,site_name):
     settings_path=source_folder+'/superlists/settings.py'
     sed(settings_path,'DEBUG = True','DEBUG = False')
     sed(settings_path,'ALLOWED_HOSTS =.+$',f'ALLOWED_HOSTS = ["{site_name}"]')
-    secret_key_file=source_folder+'superlists/secret_key.py'
+    secret_key_file=source_folder+'/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars= 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+{}":'
-        key= ''.join(random.SystemRandom().choice(chars) for _ in rang(50))
+        key= ''.join(random.SystemRandom().choice(chars) for _ in range(50))
         append(secret_key_file,f'SECRET_KEY = {key}')
     append(settings_path,'\nfrom .secret_key_file import SECRET_FILE')    
     
@@ -43,7 +43,7 @@ def _update_virtualenv(source_folder):
         run(f'python3.6 -m venv {virtualenv_folder}')
     run(f'{virtualenv_folder}/bin/pip install -r {source_folder}/requirements.txt')
 def _update_static_files(source_folder):
-    run(f'cd {source_folder} && ../virtualenv/bin/python manage.py collectstatic')
+    run(f'cd {source_folder} && ../virtualenv/bin/python manage.py collectstatic -c --noinput')
     
 def _update_database(source_folder):
     run(f'cd {source_folder} && ../virtualenv/bin/python manage.py migrate --noinput')    
