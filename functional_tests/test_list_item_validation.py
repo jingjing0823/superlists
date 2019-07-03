bdb28d7f9755a38d1dba5f3aa5e5a783a66e9b1b
lijing <lijing1@syswin.com>
2019-07-03 16:41:00 +0800

    finish chapture 16

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -2,6 +2,8 @@ from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
     def test_cannot_add_empty_list_items(self):
         #李小姐访问首页，不小心提交了一个空的代办事项
         #输入框中没有填写任何内容，她就按下了回车键
@@ -49,7 +51,21 @@ class ItemValidationTest(FunctionalTest):
         
         #李小姐看到了一个帮助消息，提示不能重复
         
-        self.wait_for(lambda:self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,"you've already get this in your list"))
+        self.wait_for(lambda:self.assertEqual(self.get_error_element().text,"you've already get this in your list"))
         
- 
+    def test_error_message_are_cleared_on_input(self):
+        #李小姐创建一个清单，但是方法不当，出现了一个错误
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Banter too thick")
+        
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        
+        self.wait_for(lambda:self.assertTrue(self.get_error_element().is_displayed()))
+        #她在输入框中输入了别的内容
+        self.get_item_input_box().send_keys('Banter too thick')
+        #李小姐看到重复提示消失了
+        self.wait_for(lambda:self.assertFalse(self.get_error_element().is_displayed()))
 
